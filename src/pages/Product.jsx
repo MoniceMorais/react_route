3afb8a149308c4a90321d50494c22ce88cda7f59
Monice Morais <monice.s.morais@gmail.com>
2023-11-20 15:45:03 -0300

    Carregamento dinâmico de dados

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
+    // rota dinâmica
     const { id } = useParams();
 
+    // carregamento dinâmico de dados
+    const url = 'http://localhost:3500/products/' + id;
+    const { data: product, loading, error } = useFetch(url);
+
     return <>
-        <p>id do produto: {id}</p>
+        {error && <p>Ocorreu um erro</p>}
+        {loading && <p>Carregando...</p>}
+        {product && (
+            <div>
+                <h1>{product.name}</h1>
+                <p>R$ {product.price}</p>
+            </div>
+        )}
+        
     </>
 }
 
