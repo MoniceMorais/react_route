3c37146bd39894ad9482104dfa6815de9d872101
Monice Morais <monice.s.morais@gmail.com>
2023-11-20 21:10:13 -0300

    Search Params

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Components
 import Navbar from './components/Navbar';
+import SearchForm from './components/SearchForm';
 
 // Pages
 import Home from './pages/Home';
@@ -12,6 +13,7 @@ import About from './pages/About';
 import Product from './pages/Product';
 import Info from './pages/info';
 import NotFound from './pages/NotFound';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -19,13 +21,24 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar />
+
+        {/* search */}
+        <SearchForm />
+
         <Routes>
           <Route path='/' element={<Home />} />
+
           <Route path='/about' element={<About />} />
+
           {/* rota dinâmica */}
           <Route path='/products/:id' element={<Product />} />
+
           {/* nested route */}
           <Route path='/products/:id/info' element={<Info />} />
+
+          {/* search */}
+          <Route path='search' element={<Search />} />
+
           {/* no match route */}
           <Route path='*' element={<NotFound />} />
         </Routes>
